42757059b5ae8b1a323c785c9eaff44cb42a64ff
Patrick Arlt <patrick.arlt@gmail.com>
2011-12-08 09:21:01 -0800

    merge changes made in Geoloqi-Website

--- js/libs/bootstrap/bootstrap-map-sizer.js
+++ js/libs/bootstrap/bootstrap-map-sizer.js
@@ -1,48 +1,54 @@
-(function( $ ){
-    
-    var defaults = {
-      width: '100%',
-      offBottom: 0,
-      offWidth: 0
-    }
+(function ($) {
 
-    var settings = {}
-    
-    var methods = {
-      init: function(options){
-        settings = $.extend({}, defaults, options);
+  var defaults = {
+    width: '100%',
+    offBottom: 0,
+    offWidth: 0,
+    callback: null
+  };
 
-        var $this = $(this),
-            self = $this; 
+  var settings = {};
+  
+  var methods = {
+    init: function(options) {
+      settings = $.extend({}, defaults, options);
 
-        $(window).bind('resize.mapSizer', function(){
-          self.mapSizer("resize");
+      var $this = $(this),
+          self = $this;
+
+      $(window).bind('resize scroll', function() {
+        self.mapSizer("resize");
+      });
+     
+      $this.mapSizer('resize');
+    },
+
+    resize: function(options) {
+      options = (typeof options == "undefined") ? {} : options;
+      settings = $.extend({}, settings, options);
+      return $(this).each(function() {
+        $(this).height(function() {
+          return ($(window).height() - $(this).offset().top - settings.offBottom);
+        }).width(function(){
+          return (settings.width - settings.offWidth);
+        }).each(function(){
+          if(settings.callback){
+            settings.callback.apply(this);        
+          }
         });
-       
-        $this.mapSizer('resize');
-      },
-
-      resize: function() {
-        return $(this).each(function(){
-          $(this).height(function(){
-            return ($('html').height() - settings.offBottom);
-          }).width(function(){
-            return (settings.width - settings.offWidth)
-          });
-        });
-      }
+      });
     }
+  };
 
-    $.fn.mapSizer = function( method ) {
-      // Method calling logic
-      if ( methods[method] ) {
-        return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-      } else if ( typeof method === 'object' || ! method ) {
-        return methods.init.apply( this, arguments );
-      } else {
-        $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
-      }    
+  $.fn.mapSizer = function( method ) {
+    if (methods[method]) {
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if (typeof method === 'object' || ! method) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+    }    
 
-    };
+  };
 
-})( jQuery );
+})(jQuery);
